924e4479391f3b952ea5927f0b67f7480f217108
Syed Omair <syedkhalidomair@gmail.com>
2025-05-13 20:27:14 +0500

    working version

--- istio/apply.go
+++ istio/apply.go
@@ -4,133 +4,20 @@ import (
 	"context"
 	"fmt"
 	"log"
+	"strconv"
+	"time"
 
 	"github.com/syedomair/istio-config-manager/config"
-	"github.com/syedomair/istio-config-manager/kube"
+	"google.golang.org/protobuf/types/known/durationpb"
+	"google.golang.org/protobuf/types/known/wrapperspb"
 	networkingv1beta1 "istio.io/api/networking/v1beta1"
-	v1beta1 "istio.io/client-go/pkg/apis/networking/v1beta1"
 	istioclientset "istio.io/client-go/pkg/clientset/versioned"
+	k8serrors "k8s.io/apimachinery/pkg/api/errors"
 	metaV1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+
+	clientnetworkingv1beta1 "istio.io/client-go/pkg/apis/networking/v1beta1"
 )
 
-func ApplyConfig(clients *kube.Clients, cfg config.AppConfig) error {
-
-	if !cfg.SkipVirtualService {
-		vs := &v1beta1.VirtualService{
-			ObjectMeta: metaV1.ObjectMeta{
-				Name:      cfg.VirtualServiceName,
-				Namespace: cfg.Namespace,
-			},
-			Spec: networkingv1beta1.VirtualService{
-				Hosts:    []string{cfg.Host},
-				Gateways: []string{"mesh"},
-				Http: []*networkingv1beta1.HTTPRoute{
-					{
-						Route: []*networkingv1beta1.HTTPRouteDestination{
-							{
-								Destination: &networkingv1beta1.Destination{
-									Host: cfg.Destination,
-								},
-							},
-						},
-					},
-				},
-			},
-		}
-
-		vsClient := clients.Istio.NetworkingV1beta1().VirtualServices(cfg.Namespace)
-		_, err := vsClient.Create(context.Background(), vs, metaV1.CreateOptions{})
-		if err != nil {
-			return fmt.Errorf("failed to create VirtualService: %w", err)
-		}
-		fmt.Printf("VirtualService %s created in namespace %s\n", cfg.VirtualServiceName, cfg.Namespace)
-	} else {
-		fmt.Println("Skipping VirtualService creation (SKIP_VIRTUAL_SERVICE is set)")
-	}
-
-	if !cfg.SkipDestinationRule {
-		dr := v1beta1.DestinationRule{
-			ObjectMeta: metaV1.ObjectMeta{
-				Name:      cfg.VirtualServiceName + "-dr",
-				Namespace: cfg.Namespace,
-			},
-			Spec: networkingv1beta1.DestinationRule{
-				Host: cfg.Destination,
-				Subsets: []*networkingv1beta1.Subset{
-					{
-						Name: cfg.SubsetName,
-						Labels: map[string]string{
-							"version": cfg.SubsetName,
-						},
-					},
-				},
-			},
-		}
-
-		_, err := clients.Istio.NetworkingV1beta1().DestinationRules(cfg.Namespace).
-			Create(context.Background(), &dr, metaV1.CreateOptions{})
-		if err != nil {
-			return fmt.Errorf("failed to create DestinationRule: %w", err)
-		}
-		fmt.Printf("DestinationRule %s created in namespace %s\n", dr.Name, cfg.Namespace)
-	} else {
-		fmt.Println("Skipping DestinationRule creation (SKIP_DESTINATION_RULE is set)")
-	}
-
-	return nil
-}
-
-/*
-	func ApplyHeaderOverride(client istioclientset.Interface, cfg config.AppConfig) error {
-		vsClient := client.NetworkingV1alpha3().VirtualServices(cfg.Namespace)
-
-		// Get existing VirtualService
-		vs, err := vsClient.Get(context.Background(), cfg.VirtualServiceName, metaV1.GetOptions{})
-		if err != nil {
-			return fmt.Errorf("failed to get VirtualService: %v", err)
-		}
-
-		log.Println("Original VirtualService fetched. Updating match rules...")
-
-		// Add or override match rule to route only requests with header x-user-type: beta
-		newMatch := &networkingv1beta1.HTTPMatchRequest{
-			Headers: map[string]*networkingv1beta1.StringMatch{
-				"x-user-type": {MatchType: &networkingv1beta1.StringMatch_Exact{Exact: "beta"}},
-			},
-			Uri: &networkingv1beta1.StringMatch{
-				MatchType: &networkingv1beta1.StringMatch_Prefix{
-					Prefix: "/user/users",
-				},
-			},
-		}
-
-		newRoute := networkingv1beta1.HTTPRoute{
-			Match: []*networkingv1beta1.HTTPMatchRequest{newMatch},
-			Rewrite: &networkingv1beta1.HTTPRewrite{
-				Uri: "/v2/users"},
-			Route: []*networkingv1beta1.HTTPRouteDestination{
-				{
-					Destination: &networkingv1beta1.Destination{
-						Host:   cfg.Host,
-						Subset: cfg.SubsetName, // e.g., v2
-					},
-				},
-			},
-		}
-
-		// Prepend new match rule to HTTP routes (so it takes priority)
-		vs.Spec.Http = append([]*networkingv1beta1.HTTPRoute{&newRoute}, vs.Spec.Http...)
-
-		// Apply the updated VirtualService
-		_, err = vsClient.Update(context.Background(), vs, metaV1.UpdateOptions{})
-		if err != nil {
-			return fmt.Errorf("failed to update VirtualService: %v", err)
-		}
-
-		log.Printf("Updated VirtualService %s with new header-based routing\n", cfg.VirtualServiceName)
-		return nil
-	}
-*/
 func ApplyHeaderOverride(client istioclientset.Interface, cfg config.AppConfig) error {
 	vsClient := client.NetworkingV1alpha3().VirtualServices(cfg.Namespace)
 
@@ -208,7 +95,7 @@ func ApplyHeaderOverride(client istioclientset.Interface, cfg config.AppConfig) 
 	return nil
 }
 
-func ApplyTrafficSplit(client istioclientset.Interface, cfg config.AppConfig, v1Weight, v2Weight int32) error {
+func ApplyTrafficSplit(client istioclientset.Interface, cfg config.AppConfig) error {
 	vsClient := client.NetworkingV1alpha3().VirtualServices(cfg.Namespace)
 
 	vs, err := vsClient.Get(context.Background(), cfg.VirtualServiceName, metaV1.GetOptions{})
@@ -216,30 +103,67 @@ func ApplyTrafficSplit(client istioclientset.Interface, cfg config.AppConfig, v1
 		return fmt.Errorf("failed to get VirtualService: %v", err)
 	}
 
-	log.Println("Original VirtualService fetched. Applying A/B traffic split...")
+	log.Println("Original VirtualService fetched. Checking for existing traffic split route...")
 
-	// Define weighted routing
-	splitRoute := &networkingv1beta1.HTTPRoute{
-		Route: []*networkingv1beta1.HTTPRouteDestination{
-			{
-				Destination: &networkingv1beta1.Destination{
-					Host:   cfg.Host,
-					Subset: "v1",
-				},
-				Weight: v1Weight,
-			},
-			{
-				Destination: &networkingv1beta1.Destination{
-					Host:   cfg.Host,
-					Subset: "v2",
-				},
-				Weight: v2Weight,
-			},
-		},
+	found := false
+
+	v1Weight, err := strconv.ParseInt(cfg.TrafficWeight1, 10, 32)
+	if err != nil {
+		return fmt.Errorf("invalid integer value given for trafiic weight 1: %v", err)
+	}
+	v1Weight32 := int32(v1Weight)
+
+	v2Weight, err := strconv.ParseInt(cfg.TrafficWeight2, 10, 32)
+	if err != nil {
+		return fmt.Errorf("invalid integer value given for trafiic weight 2: %v", err)
+	}
+	v2Weight32 := int32(v2Weight)
+
+	for _, httpRoute := range vs.Spec.Http {
+		// Match route with 2 destinations for same host with v1 and v2 subsets
+		if len(httpRoute.Route) == 2 &&
+			httpRoute.Route[0].Destination.Host == cfg.Host &&
+			httpRoute.Route[1].Destination.Host == cfg.Host {
+
+			subset1 := httpRoute.Route[0].Destination.Subset
+			subset2 := httpRoute.Route[1].Destination.Subset
+
+			if (subset1 == "v1" && subset2 == "v2") || (subset1 == "v2" && subset2 == "v1") {
+				// Update weights
+				httpRoute.Route[0].Weight = v1Weight32
+				httpRoute.Route[1].Weight = v2Weight32
+				found = true
+				log.Println("Existing traffic split rule found. Updating weights...")
+				break
+			}
+		}
 	}
 
-	// Replace entire HTTP block (assumes A/B routing takes priority)
-	vs.Spec.Http = []*networkingv1beta1.HTTPRoute{splitRoute}
+	if !found {
+		log.Println("No existing traffic split found. Adding new A/B routing rule...")
+
+		newSplitRoute := &networkingv1beta1.HTTPRoute{
+			Route: []*networkingv1beta1.HTTPRouteDestination{
+				{
+					Destination: &networkingv1beta1.Destination{
+						Host:   cfg.Host,
+						Subset: "v1",
+					},
+					Weight: v1Weight32,
+				},
+				{
+					Destination: &networkingv1beta1.Destination{
+						Host:   cfg.Host,
+						Subset: "v2",
+					},
+					Weight: v2Weight32,
+				},
+			},
+		}
+
+		// Append to existing routes
+		vs.Spec.Http = append(vs.Spec.Http, newSplitRoute)
+	}
 
 	_, err = vsClient.Update(context.Background(), vs, metaV1.UpdateOptions{})
 	if err != nil {
@@ -249,4 +173,265 @@ func ApplyTrafficSplit(client istioclientset.Interface, cfg config.AppConfig, v1
 	log.Printf("Updated VirtualService %s with v1:%d%%, v2:%d%% traffic split\n",
 		cfg.VirtualServiceName, v1Weight, v2Weight)
 	return nil
+}
+func ApplyDelayFault(client istioclientset.Interface, cfg config.AppConfig) error {
+	vsClient := client.NetworkingV1alpha3().VirtualServices(cfg.Namespace)
+
+	vs, err := vsClient.Get(context.Background(), cfg.VirtualServiceName, metaV1.GetOptions{})
+	if err != nil {
+		return err
+	}
+
+	faultDelayMillis, err := strconv.ParseInt(cfg.FaultDelayMillis, 10, 32)
+	if err != nil {
+		return fmt.Errorf("invalid integer value given for FaultDelayMillis : %v", err)
+	}
+
+	faultMatch := &networkingv1beta1.HTTPMatchRequest{
+		Headers: map[string]*networkingv1beta1.StringMatch{
+			"x-user-type": {
+				MatchType: &networkingv1beta1.StringMatch_Exact{Exact: "test"},
+			},
+		},
+	}
+
+	faultRoute := &networkingv1beta1.HTTPRoute{
+		Match: []*networkingv1beta1.HTTPMatchRequest{faultMatch},
+		Fault: &networkingv1beta1.HTTPFaultInjection{
+			Delay: &networkingv1beta1.HTTPFaultInjection_Delay{
+				Percent: 100,
+				HttpDelayType: &networkingv1beta1.HTTPFaultInjection_Delay_FixedDelay{
+					FixedDelay: durationpb.New(time.Millisecond * time.Duration(faultDelayMillis)),
+				},
+			},
+		},
+		Route: []*networkingv1beta1.HTTPRouteDestination{
+			{
+				Destination: &networkingv1beta1.Destination{
+					Host:   cfg.Host,
+					Subset: cfg.SubsetName,
+				},
+			},
+		},
+	}
+
+	vs.Spec.Http = append([]*networkingv1beta1.HTTPRoute{faultRoute}, vs.Spec.Http...)
+
+	_, err = vsClient.Update(context.Background(), vs, metaV1.UpdateOptions{})
+	log.Printf("Updated VirtualService %s with with fault\n", cfg.VirtualServiceName)
+	return err
+}
+
+func CreateTrafficMirroring(client istioclientset.Interface, cfg config.AppConfig) error {
+
+	vsClient := client.NetworkingV1alpha3().VirtualServices(cfg.Namespace)
+
+	vs, err := vsClient.Get(context.Background(), cfg.VirtualServiceName, metaV1.GetOptions{})
+	if err != nil {
+		return err
+	}
+
+	mirrorRoute := &networkingv1beta1.HTTPRoute{
+		Route: []*networkingv1beta1.HTTPRouteDestination{
+			{
+				Destination: &networkingv1beta1.Destination{
+					Host:   cfg.Host,
+					Subset: cfg.SubsetName,
+				},
+			},
+		},
+		Mirror: &networkingv1beta1.Destination{
+			Host:   cfg.Host,
+			Subset: cfg.Destination,
+		},
+	}
+
+	vs.Spec.Http = append([]*networkingv1beta1.HTTPRoute{mirrorRoute}, vs.Spec.Http...)
+	_, err = vsClient.Update(context.Background(), vs, metaV1.UpdateOptions{})
+	log.Printf("Updated VirtualService %s with with mirror\n", cfg.VirtualServiceName)
+	return err
+}
+
+func ApplyTimeout(client istioclientset.Interface, cfg config.AppConfig) error {
+
+	vsClient := client.NetworkingV1alpha3().VirtualServices(cfg.Namespace)
+
+	vs, err := vsClient.Get(context.Background(), cfg.VirtualServiceName, metaV1.GetOptions{})
+	if err != nil {
+		return err
+	}
+
+	timeoutDuration, err := strconv.ParseInt(cfg.TimeoutDuration, 10, 32)
+	if err != nil {
+		return fmt.Errorf("invalid integer value given for trafiic weight 1: %v", err)
+	}
+
+	for _, httpRoute := range vs.Spec.Http {
+		if len(httpRoute.Match) > 0 && httpRoute.Match[0].Uri != nil && httpRoute.Match[0].Uri.GetPrefix() == "/user/users" {
+			log.Printf("Updating timeout for route to %v\n", timeoutDuration)
+			httpRoute.Timeout = durationpb.New(time.Duration(timeoutDuration) * time.Second)
+		}
+	}
+
+	_, err = vsClient.Update(context.Background(), vs, metaV1.UpdateOptions{})
+	if err != nil {
+		return fmt.Errorf("failed to update VirtualService with timeout: %v", err)
+	}
+
+	log.Printf("Timeout set to %v for VirtualService %s.\n", timeoutDuration, cfg.VirtualServiceName)
+	return nil
+}
+
+func ApplyRetryPolicy(client istioclientset.Interface, cfg config.AppConfig) error {
+
+	vsClient := client.NetworkingV1alpha3().VirtualServices(cfg.Namespace)
+
+	vs, err := vsClient.Get(context.Background(), cfg.VirtualServiceName, metaV1.GetOptions{})
+	if err != nil {
+		return err
+	}
+
+	retryAttempt, err := strconv.ParseInt(cfg.RetryAttempt, 10, 32)
+	if err != nil {
+		return fmt.Errorf("invalid integer value given for RetryAttempt : %v", err)
+	}
+	retryAttempt32 := int32(retryAttempt)
+
+	retryPerTryTimeout, err := strconv.ParseInt(cfg.RetryPerTryTimeout, 10, 32)
+	if err != nil {
+		return fmt.Errorf("invalid integer value given for retryPerTryTimeout : %v", err)
+	}
+
+	retryPerTryTimeout32 := int32(retryPerTryTimeout)
+
+	for _, httpRoute := range vs.Spec.Http {
+		if len(httpRoute.Match) > 0 && httpRoute.Match[0].Uri != nil && httpRoute.Match[0].Uri.GetPrefix() == "/user/users" {
+			log.Printf("Updating retry policy to \n")
+
+			httpRoute.Retries = &networkingv1beta1.HTTPRetry{
+				Attempts:      retryAttempt32,
+				PerTryTimeout: durationpb.New(time.Duration(retryPerTryTimeout32) * time.Second),
+				RetryOn:       cfg.RetryOn,
+			}
+		}
+	}
+
+	_, err = vsClient.Update(context.Background(), vs, metaV1.UpdateOptions{})
+	if err != nil {
+		return fmt.Errorf("failed to update VirtualService with timeout: %v", err)
+	}
+
+	log.Printf("Retries policy set for VirtualService %s.\n", cfg.VirtualServiceName)
+	return nil
+}
+
+/*
+func ApplyCircuitBreaker(client istioclientset.Interface, cfg config.AppConfig) error {
+
+		drClient := client.NetworkingV1beta1().DestinationRules(cfg.Namespace)
+
+		maxEjectionPercent := 100
+		maxEjectionPercent32 := int32(maxEjectionPercent)
+		consecutiveGatewayErrors := 1
+		consecutiveGatewayErrors32 := int32(consecutiveGatewayErrors)
+
+		dr := &networkingv1beta1.DestinationRule{
+			Host: cfg.Host,
+			TrafficPolicy: &networkingv1beta1.TrafficPolicy{
+				ConnectionPool: &networkingv1beta1.ConnectionPoolSettings{
+					Tcp: &networkingv1beta1.ConnectionPoolSettings_TCPSettings{
+						MaxConnections: 1,
+					},
+					Http: &networkingv1beta1.ConnectionPoolSettings_HTTPSettings{
+						Http1MaxPendingRequests:  1,
+						MaxRequestsPerConnection: 1,
+					},
+				},
+				OutlierDetection: &networkingv1beta1.OutlierDetection{
+					ConsecutiveGatewayErrors: wrapperspb.UInt32(uint32(consecutiveGatewayErrors32)),
+					Interval:                 prototime("5s"),
+					BaseEjectionTime:         prototime("30s"),
+					MaxEjectionPercent:       maxEjectionPercent32,
+				},
+			},
+		}
+
+
+
+		_, err := drClient.Create(context.Background(), dr, metaV1.CreateOptions{})
+		if err != nil {
+			if k8serrors.IsAlreadyExists(err) {
+				log.Printf("DestinationRule %s already exists. Skipping creation.\n", cfg.Destination)
+				return nil
+			}
+			return fmt.Errorf("failed to create DestinationRule: %v", err)
+		}
+
+		log.Printf("DestinationRule %s created with circuit breaker settings.\n", cfg.Destination)
+		return nil
+	}
+
+	func prototime(duration string) *durationpb.Duration {
+		d, err := time.ParseDuration(duration)
+		if err != nil {
+			return nil
+		}
+		return durationpb.New(d)
+	}
+*/
+func ApplyCircuitBreaker(client istioclientset.Interface, cfg config.AppConfig) error {
+	drClient := client.NetworkingV1beta1().DestinationRules(cfg.Namespace)
+
+	maxEjectionPercent := 100
+	consecutiveGatewayErrors := 1
+
+	// Create the API version of DestinationRule
+	apiDr := &networkingv1beta1.DestinationRule{
+		Host: cfg.Host,
+		TrafficPolicy: &networkingv1beta1.TrafficPolicy{
+			ConnectionPool: &networkingv1beta1.ConnectionPoolSettings{
+				Tcp: &networkingv1beta1.ConnectionPoolSettings_TCPSettings{
+					MaxConnections: 1,
+				},
+				Http: &networkingv1beta1.ConnectionPoolSettings_HTTPSettings{
+					Http1MaxPendingRequests:  1,
+					MaxRequestsPerConnection: 1,
+				},
+			},
+			OutlierDetection: &networkingv1beta1.OutlierDetection{
+				ConsecutiveGatewayErrors: wrapperspb.UInt32(uint32(consecutiveGatewayErrors)),
+				Interval:                 prototime("5s"),
+				BaseEjectionTime:         prototime("30s"),
+				MaxEjectionPercent:       int32(maxEjectionPercent),
+			},
+		},
+	}
+
+	// Convert to the client-go version
+	clientDr := &clientnetworkingv1beta1.DestinationRule{
+		ObjectMeta: metaV1.ObjectMeta{
+			Name: cfg.Destination, // Make sure to set the name
+		},
+		Spec: *apiDr,
+	}
+
+	_, err := drClient.Create(context.Background(), clientDr, metaV1.CreateOptions{})
+	if err != nil {
+		if k8serrors.IsAlreadyExists(err) {
+			log.Printf("DestinationRule %s already exists. Skipping creation.\n", cfg.Destination)
+			return nil
+		}
+		return fmt.Errorf("failed to create DestinationRule: %v", err)
+	}
+
+	log.Printf("DestinationRule %s created with circuit breaker settings.\n", cfg.Destination)
+	return nil
+}
+
+func prototime(duration string) *durationpb.Duration {
+	d, err := time.ParseDuration(duration)
+	if err != nil {
+		return nil
+	}
+	return durationpb.New(d)
 }
